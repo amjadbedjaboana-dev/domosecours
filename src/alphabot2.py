0692053bc61b334e20c1304c454d7da70a075b91
amjadbedjaboana-dev <amjadbedjaboana@gmail.com>
2026-07-07 22:47:31 +0200

    Add files via upload

---
+++ src/alphabot2.py
@@ -0,0 +1,58 @@
+import RPi.GPIO as GPIO
+import time
+
+class AlphaBot2(object):
+
+    def __init__(self, ain1=12, ain2=13, ena=6, bin1=20, bin2=21, enb=26):
+        self.AIN1 = ain1
+        self.AIN2 = ain2
+        self.BIN1 = bin1
+        self.BIN2 = bin2
+        self.ENA = ena
+        self.ENB = enb
+        self.PA = 25  # Vitesse par défaut
+        self.PB = 25
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        GPIO.setup(self.AIN1, GPIO.OUT)
+        GPIO.setup(self.AIN2, GPIO.OUT)
+        GPIO.setup(self.BIN1, GPIO.OUT)
+        GPIO.setup(self.BIN2, GPIO.OUT)
+        GPIO.setup(self.ENA, GPIO.OUT)
+        GPIO.setup(self.ENB, GPIO.OUT)
+        self.PWMA = GPIO.PWM(self.ENA, 500)
+        self.PWMB = GPIO.PWM(self.ENB, 500)
+        self.PWMA.start(self.PA)
+        self.PWMB.start(self.PB)
+        self.stop()
+
+    def forward(self):
+        GPIO.output(self.AIN1, GPIO.LOW)
+        GPIO.output(self.AIN2, GPIO.HIGH)
+        GPIO.output(self.BIN1, GPIO.LOW)
+        GPIO.output(self.BIN2, GPIO.HIGH)
+
+    def right(self):
+        GPIO.output(self.AIN1, GPIO.LOW)
+        GPIO.output(self.AIN2, GPIO.HIGH)
+        GPIO.output(self.BIN1, GPIO.LOW)
+        GPIO.output(self.BIN2, GPIO.LOW)
+
+    def left(self):
+        GPIO.output(self.AIN1, GPIO.LOW)
+        GPIO.output(self.AIN2, GPIO.LOW)
+        GPIO.output(self.BIN1, GPIO.LOW)
+        GPIO.output(self.BIN2, GPIO.HIGH)
+
+    def backward(self):
+        GPIO.output(self.AIN1, GPIO.HIGH)
+        GPIO.output(self.AIN2, GPIO.LOW)
+        GPIO.output(self.BIN1, GPIO.HIGH)
+        GPIO.output(self.BIN2, GPIO.LOW)
+
+    def stop(self):
+        GPIO.output(self.AIN1, GPIO.LOW)
+        GPIO.output(self.AIN2, GPIO.LOW)
+        GPIO.output(self.BIN1, GPIO.LOW)
+        GPIO.output(self.BIN2, GPIO.LOW)
